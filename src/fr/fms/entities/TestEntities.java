a43520cf1a9ddb70b63e8c9a33aee9b778f9feda
Philippe P <philippe.peyrous@fms-ea.com>
2026-08-18 13:28:06 +0200

    exo 3.6 ok

---
+++ src/fr/fms/entities/TestEntities.java
@@ -0,0 +1,32 @@
+package fr.fms.entities;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestEntities {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employe[] team = new Employe[5];
+		
+		
+		team[0] = new EmployeSalary("Paris", "France",5000, "Leponge", "Bob", 36, "Elysee à Paris", "Atlantique", 999999.0);		
+		team[1] = new EmployeCommission("Washington", "USA", 7600000, "Gates", "Bill", 70, "USA", "Fondation B&M", 30);
+		team[2] = new EmployeCommission("Pretoria", "Afrique du Sud", 800000, "Musk", "Elon", 55, "Palo Alto", "Tesla", 45);
+		team[3] = new EmployeSalary("Pau", "France du Sud", 100000, "Cabanne", "Olivier", 25, "Pau", "Total", 50000);
+		team[4] = new EmployeSalary("Tarbes", "France du Sud", 45000, "Laborde", "Colline", 30, "Tarbes", "Alstom", 30000);
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println(team[i]);
+		}
+		
+		List<Employe> teamList = Arrays.asList(team);
+		
+		for(Employe element : team) {
+			System.out.println(element);
+		}
+		
+	}
+
+}
